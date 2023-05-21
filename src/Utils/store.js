6cbb0d38371139c2e3efcb617ec000857b7b681e
Shivam Goel <shivamgoel150@gmail.com>
2023-05-21 18:12:08 +0530

    cache of search query results (optimization )

--- src/Utils/store.js
+++ src/Utils/store.js
@@ -1,9 +1,11 @@
 import appSlice from "./appSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import seachSlice from "./seachSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
+    search:seachSlice,
   },
 });
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../Utils/appSlice";
 import headerIcon from "../svg/headerIcon.svg";
 import searchIcon from "../svg/searchIcon.svg";
 import userIcon from "../svg/userIcon.svg";
+import { cacheResult } from "../Utils/seachSlice";
+
 const Header = () => {
   const [searchSuggestion, setSearchSuggestion] = useState("");
-  const [suggestionList, setSuggestionList]=useState([]);
-  const [showSuggestions, setShowSuggestions]=useState(false);
-
+  const [suggestionList, setSuggestionList] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const searchCache = useSelector((store) => store.search.searchCache);
+  
   const dispatch = useDispatch();
+
+  // toggle side bar 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
 
+  // function to fetch data from query 
   const getSearchSuggestions = async () => {
     const data = await fetch(
       "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" +
@@ -22,11 +28,20 @@ const Header = () => {
     const json = await data.json();
 
     setSuggestionList(json[1]);
-    console.log(suggestionList)
+    dispatch(cacheResult({[searchSuggestion]:json[1]}));
+
   };
 
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    // debouncing of search result 
+    const timer = setTimeout(() => {
+      if (searchCache[searchSuggestion]) {
+        setSuggestionList(searchCache[searchSuggestion]);
+      } else {
+        // if it not present in cache then make API call and store result in Cache
+        getSearchSuggestions();
+      }
+    }, 200);
     return () => {
       clearTimeout(timer);
     };
@@ -51,13 +66,13 @@ const Header = () => {
       <div className="flex flex-col">
         <div className="flex flex-row">
           <input
-            className="w-96 h-10 rounded-l-full  placeholder:text-gray-500 pl-4 focus:border-blue-500 "
+            className="w-96 h-10 rounded-l-full border-black placeholder:text-gray-500 pl-4 "
             type="text"
             placeholder="Search"
             onChange={(e) => setSearchSuggestion(e.target.value)}
             value={searchSuggestion}
-            onFocus={()=>setShowSuggestions(true)}
-            onBlur={()=>setShowSuggestions(false)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
 
           <img
@@ -66,33 +81,30 @@ const Header = () => {
             alt="header-icon"
           />
         </div>
-        {showSuggestions&& (suggestionList.length>0)&&(
-        <div className="fixed border-gray-400 bg-slate-50 mt-11 ml-1 w-96 rounded-xl shadow-2xl opacity-95">
-          <ul className="m-4 list-inside ">
-
-            {suggestionList.map((suggest)=>(<li className="flex items-center hover:bg-gray-200 rounded-lg p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-4 h-4 mr-2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-              {suggest}
-            </li>
-
-            ))}
-            
-           
-          </ul>
-        </div>
+        {showSuggestions && suggestionList.length > 0 && (
+          <div className="fixed border-gray-400 bg-slate-50 mt-11 ml-1 w-96 rounded-xl shadow-2xl opacity-95">
+            <ul className="m-4 list-inside ">
+              {suggestionList.map((suggest) => (
+                <li className="flex items-center hover:bg-gray-200 rounded-lg p-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 h-4 mr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                  </svg>
+                  {suggest}
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </div>
 
