1d81c279b48df0e00511dd6a3ce3c76d99430bfa
Shivam Goel <shivamgoel150@gmail.com>
2023-05-26 01:11:22 +0530

    infinite scrolling

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -2,33 +2,53 @@ import React, { useEffect, useState } from "react";
 import Comment from "./Comment";
 import { useSearchParams } from "react-router-dom";
 import { YOUTUBE_COMMENTS_LIST_API } from "../Utils/constants";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const CommentsList = () => {
   const [searchParams] = useSearchParams();
   const [allComments,setAllComments]=useState([]);
+  const [pageToken,setPageToken]=useState("");
+  const [response,setResponse]=useState({});
 
   const getAllComments = async () => {
     const data = await fetch(
       YOUTUBE_COMMENTS_LIST_API +
         searchParams.get("v") +
+        "&pageToken="+pageToken+
         "&key=" +
         process.env.REACT_APP_YOUTUBE_API_KEY
     );
     const json = await data.json();
 
-    setAllComments(json.items);
+    setAllComments([...allComments,...json?.items]);
+    setResponse(json);
   };
 
+  const getNextPageToken=()=>{
+    console.log(response?.nextPageToken)
+    setPageToken(response?.nextPageToken);
+  }
   useEffect(() => {
     getAllComments();
-  }, []);
+  }, [pageToken]);
+
+
   return (
     <div className="py-8 mx-8 w-[885px] h-[498px] ">
-      {allComments.map((comments)=>(
+      
+  <InfiniteScroll
+    dataLength={allComments.length}
+    next={()=>getNextPageToken()}
+    hasMore={true}
+    loader={<h4>Loading...</h4>}
+  >
+   {allComments.map((comments)=>(
         <Comment key={comments?.id} commentInfo={comments}/>
       ))}
-      <h3>Comments</h3>
+  </InfiniteScroll>
     </div>
+
+    
   );
 };
 
